66b49326ceea68eef98a11f9cc7f61a0d349506b
Walle 何國瑋 <135415607+walle45611@users.noreply.github.com>
2024-09-15 23:21:42 +0800

    (feat): 將 tailwind.confg.js to ts (#15)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     return (
         <>
             <div>
+                <p className="text-sky-400">The quick brown fox...</p>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
