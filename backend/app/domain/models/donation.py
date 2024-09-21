1bf05ba1fc7e3d0ac3e011674522fe82cd21b854
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-09-21 16:54:12 +0800

    (feat): 添加欄位至User, Donation表格

--- backend/app/domain/models/donation.py
+++ backend/app/domain/models/donation.py
@@ -1,17 +1,44 @@
+from enum import Enum
 from typing import Optional
 from datetime import datetime
 
-from sqlalchemy.sql import func
-from sqlalchemy import Column, DateTime
+# from sqlalchemy.sql import func
+from sqlalchemy import Column, DateTime, func
+from sqlalchemy import Enum as SQLAlchemyEnum, Column
 from sqlmodel import SQLModel, Field, Relationship
 
+class PublicStatus(str, Enum):
+   FULL_PUBLIC = "FULL_PUBLIC"
+   PARTIALLY_PUBLIC = "PARTIALLY_PUBLIC"
+   NOT_PUBLIC = "NOT_PUBLIC"
+
 class Donation(SQLModel, table=True):
+    """
+    Represents a donation record in the system.
+
+    Attributes:
+        donation_id (int | None): The unique identifier for the donation.
+        amount (Optional[float]): 捐款金額
+        currency (str): 捐款貨幣類型
+        payment_method (str): 支付方式
+        payment_status (str): 支付狀態
+        transaction_id (str): 金流平台返還ID
+        public_status (Optional[PublicStatus]): 狀態是否公開
+        created_at (datetime): The timestamp when the donation was created.
+        updated_at (datetime): The timestamp when the donation was last updated.
+    """
+
     donation_id: int | None = Field(default=None, primary_key=True)
     amount: Optional[float] = None
     currency: str
     payment_method: str
     payment_status: str
     transaction_id: str
+    public_status: Optional[PublicStatus] = Field(
+        sa_column=Column(
+            SQLAlchemyEnum(PublicStatus, name="publicstatus", create_type=True)
+        )
+    )
 
     created_at: datetime = Field(
         sa_column=Column(DateTime(timezone=True), server_default=func.now())
--- backend/app/domain/models/user.py
+++ backend/app/domain/models/user.py
@@ -1,22 +1,50 @@
+# from enum import Enum
 from typing import Optional, List
-from datetime import datetime, timezone
+from datetime import datetime, date
 
 from sqlmodel import SQLModel, Field, Relationship
+from sqlalchemy import Column, DateTime, func
+# from sqlalchemy import Enum as Column
 
 from app.domain.models.donation import Donation
 
 class User(SQLModel, table=True):
+    """
+    User model representing a user in the system.
+
+    Attributes:
+        user_id (int | None): The unique identifier for the user. Primary key.
+        user_name (str): 使用者名稱
+        user_birthday (Optional[date]): 使用者生日
+        user_id_card (str): 使用者身分證字號
+        user_phone_number (Optional[int]): 使用者電話號碼
+        user_email (str): 使用者電子郵件
+        user_identity (str): 使用者身分
+        user_household_address (str): 使用者的戶籍地址
+        user_correspondence_address (str): 使用者的通訊地址
+        created_at (datetime): The timestamp when the user was created.
+        updated_at (datetime): The timestamp when the user was last updated.
+        donations (List["Donation"]): The list of donations associated with the user.
+    """
     user_id: int | None = Field(default=None, primary_key=True)
     user_name: str
-    user_email: str
+    user_birthday: Optional[date] = None
+    user_id_card: str
     user_phone_number: Optional[int] = None
-    user_address: str
-    created_at: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=None)
+    user_email: str
+    user_identity: str
+    user_household_address: str
+    user_correspondence_address: str
+
+    created_at: datetime = Field(
+        sa_column=Column(DateTime(timezone=True), server_default=func.now())
     )
-    updated_at: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=None),
-        sa_column_kwargs={"onupdate": datetime.now(timezone.utc).replace(tzinfo=None)}
+    updated_at: datetime = Field(
+        sa_column=Column(
+            DateTime(timezone=True),
+            server_default=func.now(),
+            onupdate=func.now()
+        )
     )
 
     donations: List["Donation"] = Relationship(back_populates="user")  # 修正 back_populates
