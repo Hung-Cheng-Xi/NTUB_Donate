01b5dcea3da4434b56015e5c670e70a982fd6ba0
Walle 何國瑋 <135415607+walle45611@users.noreply.github.com>
2024-09-28 17:41:07 +0800

    (fix): 解決 Model 設定錯誤問題 (#6)
    error message : One or more mappers failed to initialize - can't proceed with initialization of other mappers.

--- backend/app/domain/models/unit.py
+++ backend/app/domain/models/unit.py
@@ -1,17 +1,18 @@
-from __future__ import annotations
 from typing import TYPE_CHECKING, List
 from sqlmodel import SQLModel, Field, Relationship
 
-from app.domain.models.donation_purpose import DonationPurpose
 
 if TYPE_CHECKING:
+    from app.domain.models.donation_purpose import DonationPurpose
     from app.domain.models.news import News
 
 
 class Unit(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True)
     name: str
-
-    news: List["News"] = Relationship(back_populates="unit")
+    news: List["News"] = Relationship(
+        back_populates="unit"
+    )
     donation_purposes: List["DonationPurpose"] = Relationship(
-        back_populates="unit")
+        back_populates="unit"
+    )
