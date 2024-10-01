cf20abfba28e9b3481d615aded8297a74990fadf
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-10-01 14:05:12 +0800

    (feat): 設定 log 設定以及調整 router prefix 和移除舊有 db function

--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,12 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from app.core.config import settings
-from app.core.database import create_db_and_tables
 from app.application.endpoints import main_router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logging.info("Starting the app")
-    await create_db_and_tables()
     yield
     logging.info("Shutting down the app")
 
@@ -20,7 +19,7 @@ app = FastAPI(
     redoc_url="/redoc" if settings.enable_docs else None
 )
 
-app.include_router(main_router)
+app.include_router(main_router, prefix="/api")
 
 if __name__ == "__main__":
     import uvicorn
@@ -28,5 +27,5 @@ if __name__ == "__main__":
         "main:app",
         host="0.0.0.0",
         port=8000,
-        reload=True
+        log_config="log.ini",
     )
