deffbac8e7601706b190df2fce2ff93d65edcbf2
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-10-02 15:37:56 +0800

    (feat): 更改為 Depends 注入

--- backend/app/application/endpoints/donation_purpose.py
+++ backend/app/application/endpoints/donation_purpose.py
@@ -1,9 +1,7 @@
 import logging
 from typing import List
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.database import get_db_session
 from app.domain.models.donation_purpose import DonationPurpose
 from app.application.schema.donation_purpose import DonationPurposeCreate
 from app.infrastructure.repositories.donation_purpose import DonationPurposeRepository
@@ -12,16 +10,17 @@ router = APIRouter()
 
 
 @router.get("/", response_model=List[DonationPurpose])
-async def read_donation_purposes(db: AsyncSession = Depends(get_db_session)):
-    donation_purpose_repo = DonationPurposeRepository(db)
-    donation_purposes = await donation_purpose_repo.get_all_donation_purposes()
+async def read_donation_purposes(
+    repository: DonationPurposeRepository = Depends(),
+):
     logging.info("取得 Donation Purpose 資料")
-    return donation_purposes
+    return await repository.get_all_donation_purposes()
 
 
 @router.post("/", response_model=DonationPurpose)
-async def create_donation_purpose(new_donation_purpose: DonationPurposeCreate, db: AsyncSession = Depends(get_db_session)):
-    donation_purpose_repo = DonationPurposeRepository(db)
-    created_donation_purpose = await donation_purpose_repo.create_donation_purpose(new_donation_purpose)
+async def create_donation_purpose(
+    new_donation_purpose: DonationPurposeCreate,
+    repository: DonationPurposeRepository = Depends(),
+):
     logging.info("新增 Donation Purpose 資料到資料庫")
-    return created_donation_purpose
+    return await repository.create_donation_purpose(new_donation_purpose)
