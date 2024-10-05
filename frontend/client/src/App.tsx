f406102fe97f0422c371baef465cac08b2398295
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-10-05 18:18:22 +0800

    整理前端專案

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     return (
         <>
             <div>
-                <p className="text-sky-400">The quick brown fox...</p>
+                <h1>Client</h1>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
