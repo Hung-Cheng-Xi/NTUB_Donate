48d74196e8fe1b7389c207c2d9513bb073470533
Walle 何國瑋 <135415607+walle45611@users.noreply.github.com>
2024-10-10 23:12:02 +0800

    (refactor): 重構 main.py，加邏輯放置 create_app 方法

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,10 @@ import logging
 import logging.config
 import configparser
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 from contextlib import asynccontextmanager
 
-from fastapi.routing import APIRoute
-
-from app.core.config import settings
+from app.core.settings import settings
 from app.application.client.endpoints import client_router
 from app.application.admin.endpoints import admin_router
 
@@ -28,24 +27,24 @@ async def lifespan(app: FastAPI):
 def create_app() -> FastAPI:
     configure_logging()
 
-app = FastAPI(
-    lifespan=lifespan,
-    openapi_url="/openapi.json" if settings.enable_docs else None,
-    docs_url="/docs" if settings.enable_docs else None,
-    redoc_url="/redoc" if settings.enable_docs else None,
-)
+    app = FastAPI(
+        lifespan=lifespan,
+        openapi_url="/openapi.json" if settings.enable_docs else None,
+        docs_url="/docs" if settings.enable_docs else None,
+        redoc_url="/redoc" if settings.enable_docs else None,
+    )
 
-
-def use_route_names_as_operation_ids(app: FastAPI) -> None:
     for route in app.routes:
         if isinstance(route, APIRoute):
             route.operation_id = route.name
 
+    app.include_router(client_router, prefix="/api")
+    app.include_router(admin_router, prefix="/api")
 
-app.include_router(client_router, prefix="/api")
-app.include_router(admin_router, prefix="/api")
-use_route_names_as_operation_ids(app)
+    return app
 
+
+app = create_app()
 
 if __name__ == "__main__":
     import uvicorn
