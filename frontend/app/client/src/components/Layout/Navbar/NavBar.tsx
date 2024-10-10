943a95049e7da6be3870fa39c65a7f7539b70ef4
hung <157833389+Hung-Cheng-Xi@users.noreply.github.com>
2024-10-10 16:40:38 +0800

    (feat): navbar 添加動畫左右填滿並且區分首頁與分頁 navbar (#24)

---
+++ frontend/app/client/src/components/Layout/Navbar/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import NavBarItem from './NavBarItem';
+import { NavBarProps } from '../../../types/nav';
+
+const HomeNavBar: React.FC<NavBarProps> = ({ links }) => {
+  return (
+    <div className="flex justify-center">
+      <div
+        className="fixed bg-white mx-auto flex justify-between items-center shadow-md h-[84px] z-50 w-full rounded-none"
+      >
+        <div className="flex items-center">
+          <img
+            className="w-[304px] h-[44px] my-5 ml-14"
+            src="http://i.imgur.com/2URSQrW.gif"
+            alt="國立臺北商業大學"
+          />
+        </div>
+        <NavBarItem links={links} />
+      </div>
+    </div>
+  );
+};
+
+export default HomeNavBar;
