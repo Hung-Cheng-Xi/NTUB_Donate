b75c595771546c29ead839655022b5e34cb2d2dd
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-10-12 10:49:23 +0800

    (feat): admin site 添加 login, home 頁面

--- frontend/app/admin/src/router.tsx
+++ frontend/app/admin/src/router.tsx
@@ -1,5 +1,25 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { Layout } from './components/Layout/Layout';
+import DonateLoginPage from './pages/donate.LoginPage';
+import DonateHomePage from './pages/donate.HomePage';
 
 export const router = createBrowserRouter([
-
+  {
+    path: '/admin/login/',
+    async lazy() {
+      return { Component: DonateLoginPage };
+    },
+  },
+  {
+    Component: Layout,
+    // loader: RequireAuthService,
+    children: [
+      {
+        path: '/admin/',
+        async lazy() {
+          return { Component: DonateHomePage };
+        },
+      },
+    ],
+  },
 ]);
