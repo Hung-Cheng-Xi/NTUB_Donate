f787e5e65f24062270c70f1981f2071191ebdef7
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-10-13 20:51:31 +0800

    (refactor): 調整 function 註解內容

--- backend/app/application/client/schemas/user.py
+++ backend/app/application/client/schemas/user.py
@@ -4,31 +4,26 @@ from sqlmodel import SQLModel
 
 
 class UserBase(SQLModel):
-    """
-    User 的基本 schema，包含必要的字段，如 account。
-    不包含自動生成的 id 和 created_at。
+    """User 的基本 schema，包含必要的字段，如 account。
+    不包含自動生成的 id。
     """
     account: str
 
 
 class UserCreate(UserBase):
-    """
-    用於創建 User 記錄的 schema，繼承 UserBase。
+    """用於創建 User 記錄的 schema，繼承 UserBase。
     包含用戶提交的所有必要字段。
     """
     pass
 
 
 class UserUpdate(UserBase):
-    """
-    用於更新 User 記錄的 schema，允許部分字段更新。
-    """
+    """用於更新 User 記錄的 schema，允許部分字段更新。"""
     pass
 
 
 class UserInDBBase(UserBase):
-    """
-    表示數據庫中 User 記錄的基礎結構，包含自動生成的 id 和 created_at。
+    """表示數據庫中 User 記錄的基礎結構，包含自動生成的 id。
     from_attributes 設置為 True，以允許 SQLAlchemy 模型轉換為 Pydantic 模型。
     """
     id: Optional[int]
@@ -38,8 +33,6 @@ class UserInDBBase(UserBase):
         from_attributes = True
 
 
-class User(UserInDBBase):
-    """
-    用於返回 User 的基本信息，繼承 UserInDBBase，適用於讀取操作。
-    """
+class UserInfo(UserInDBBase):
+    """用於返回 User 的基本信息，繼承 UserInDBBase，適用於讀取操作。"""
     pass
