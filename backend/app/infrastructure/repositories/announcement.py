e349144bc68d8c0955969b1bd68f11cb5e0c3f85
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-10-17 00:43:18 +0800

    （refactor）: 更改 repositories 名稱以及移除多餘 function

---
+++ backend/app/infrastructure/repositories/announcement.py
@@ -0,0 +1,71 @@
+from app.domain.models.announcement import Announcement
+from app.domain.models.unit import Unit
+from app.infrastructure.repositories.base import BaseRepository
+from app.application.utils.schemas.announcement import AnnouncementInfo
+from app.application.admin.schemas.announcement import (
+    AnnouncementCreate,
+    AnnouncementUpdate
+)
+
+from sqlalchemy.orm import joinedload
+
+
+class AnnouncementRepository(BaseRepository[Announcement]):
+    async def create_announcement(
+        self,
+        announcement_create: AnnouncementCreate,
+    ) -> Announcement:
+        """新增一筆最新消息"""
+        announcement = Announcement(**announcement_create.model_dump())
+        return await self.create_instance(announcement)
+
+    async def get_announcement(
+        self,
+        announcement_id: int,
+    ) -> Announcement:
+        """根據最新消息 ID 取得最新消息"""
+        return await self.get_by_id(announcement_id, Announcement)
+
+    async def get_announcements(
+        self,
+        skip: int,
+        limit: int,
+    ) -> list[AnnouncementInfo]:
+        """取得分頁的最新消息"""
+        # 查詢 Announcement 並加載與 unit 的關聯
+        announcements = await self.model_relations(
+            Announcement,
+            skip,
+            limit,
+            [joinedload(Announcement.unit)]
+        )
+
+        return [
+            AnnouncementInfo.model_validate(announcement)
+            for announcement in announcements
+        ]
+
+    async def update_announcement(
+        self,
+        announcement_id: int,
+        updated_announcement: AnnouncementUpdate,
+    ) -> AnnouncementUpdate:
+        """更新一筆最新消息"""
+        announcement = updated_announcement.model_dump()
+        return await self.update_instance(announcement_id, announcement, Announcement)
+
+    async def patch_announcement(
+        self,
+        announcement_id: int,
+        updated_announcement: Announcement,
+    ) -> Announcement:
+        """部分更新一筆最新消息"""
+        announcement = updated_announcement.model_dump()
+        return await self.patch_instance(announcement_id, announcement, Announcement)
+
+    async def delete_announcement(
+        self,
+        announcement_id: int,
+    ) -> bool:
+        """刪除一筆最新消息"""
+        return await self.delete_instance(announcement_id, Announcement)
---
+++ backend/app/infrastructure/repositories/regulation.py
@@ -0,0 +1,44 @@
+from app.domain.models.regulation import Regulation
+from app.infrastructure.repositories.base import BaseRepository
+from app.application.utils.schemas.regulation import RegulationInfo
+from app.application.admin.schemas.regulation import RegulationCreate
+from app.application.admin.schemas.donation_purpose import DonationPurposeUpdate
+
+
+class RegulationRepository(BaseRepository[Regulation]):
+    async def create_regulation(
+        self,
+        regulation_create: RegulationCreate,
+    ) -> Regulation:
+        """新增一筆相關法規"""
+        regulation = Regulation(**regulation_create.model_dump())
+        return await self.create_instance(regulation)
+
+    async def get_regulation(
+        self,
+        regulation_id: int,
+    ) -> Regulation:
+        """根據相關法規 ID 取得相關法規"""
+        return await self.get_by_id(regulation_id, Regulation)
+
+    async def get_regulations(
+        self,
+        skip: int,
+        limit: int,
+    ) -> list[RegulationInfo]:
+        """取得分頁的相關法規"""
+        regulations = await self.get_paginated_all(Regulation, skip, limit)
+        return [RegulationInfo.model_dump(regulation) for regulation in regulations]
+
+    async def update_regulation(
+        self,
+        regulation_id: int,
+        updated_regulation: DonationPurposeUpdate,
+    ) -> DonationPurposeUpdate:
+        """更新一筆相關法規"""
+        regulation = updated_regulation.model_dump()
+        return await self.update_instance(regulation_id, regulation, Regulation)
+
+    async def delete_regulation(self, regulation_id: int) -> bool:
+        """刪除一筆相關法規"""
+        return await self.delete_instance(regulation_id, Regulation)
