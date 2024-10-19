2bef92b5ce861d162e6cdb9b7571c864a4d6a59b
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-10-19 16:32:22 +0800

    (feat): 改為讀取 .env

--- frontend/app/admin/vite.config.ts
+++ frontend/app/admin/vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/admin/',
-    root: "app/admin",
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/admin/api/': {
-                target: 'http://120.97.28.11:8081/',
-                changeOrigin: true,
-            },
-        },
+  base: '/admin/',
+  root: 'app/admin',
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/admin/api/': {
+        // target: 'http://120.97.28.11:8081/',
+        target: process.env.VITE_API_BASE_URL, // 使用 .env 中設置的 API 基本 URL,
+        changeOrigin: true,
+      },
     },
-})
+  },
+});
