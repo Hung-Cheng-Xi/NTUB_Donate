cabe9fdcf2f8ea7388f7d8919a71d4fd3fc4d9d3
Walle 何國瑋 <135415607+walle45611@users.noreply.github.com>
2024-10-24 11:30:48 +0800

    Delete frontend/app/admin/src/services/RequireAuthService.tsx

--- frontend/app/admin/src/services/RequireAuthService.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "react-router-dom";
-
-export async function RequireAuthService() {
-    if (localStorage.getItem("access_token") === null) {
-        return redirect("/admin/login");
-    }
-    return true;
-}
